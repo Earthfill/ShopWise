f594695e3f1ade40240e31efc89281f238126adf
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2024-05-02 09:18:33 +0100

    main commit

--- data.jsx
+++ data.jsx
@@ -3,94 +3,94 @@ import { nanoid } from "nanoid";
 export const images = [
   {
     id: nanoid(),
-    logo: './src/images/apple-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/apple-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/colgate-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/colgate-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/dolby-digital-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/dolby-digital-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/dove-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/dove-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/gillette-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/gillette-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/nestle-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/nestle-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/playstation-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/playstation-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/puma-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/puma-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/purina-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/purina-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/red-bull-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/red-bull-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/rolex-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/rolex-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/samsung-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/samsung-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/sandisk-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/sandisk-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/toyota-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/toyota-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/volkswagen-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/volkswagen-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/gucci-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/gucci-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/hills-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/hills-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/hp-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/hp-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/kobe-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/kobe-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/lays-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/lays-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/acer-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/acer-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/louis-vuitton-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/louis-vuitton-logo.svg'
   },
   {
     id: nanoid(),
-    logo: './src/images/mercedes-benz-logo.svg'
+    logo: '/Users/USER/Documents/Frontend Projects/shopWise/src/images/mercedes-benz-logo.svg'
   },
 ]
